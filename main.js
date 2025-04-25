8770e6da9cdcdb1ff4f1d6f97c3bd9a9f2a25f3b
Francesco Viccica <viccicafrancesco@gmail.com>
2025-04-25 17:22:34 +0200

    esercizio svolto

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('ciao');
+function conta(numero) {
+
+for ( let i = 1; i <= numero; i++) {
+    // console.log(i);
+    
+    if (i % 15 == 0){
+        console.log(`FizzBuzz`);
+    }else if(i % 3 == 0){
+        console.log (`Fizz`);
+    }else if(i % 5 == 0){
+        console.log(`Buzz`);
+    }else{
+        console.log(i);
+    }
+}        
+
+}
+
+conta(50);
